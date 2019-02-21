d806cdca3cbd94466b87164d6767d46803919d72
Alexandre Marquet <alexandre.marquet@imt-atlantique.fr>
2019-02-21 15:49:23 +0100

    Improve CFO and delay estimation.

--- gr-lora2/python/lora_preamble_detect.py
+++ gr-lora2/python/lora_preamble_detect.py
@@ -30,7 +30,7 @@ class lora_preamble_detect(gr.sync_block):
     """
     docstring for block lora_preamble_detect
     """
-    def __init__(self, SF, preamble_len, debug=False):
+    def __init__(self, SF, preamble_len, debug=False, thres=1e-4):
         gr.sync_block.__init__(self,
             name="lora_preamble_detect",
             in_sig=[numpy.complex64],
@@ -40,15 +40,20 @@ class lora_preamble_detect(gr.sync_block):
 
         self.M=2**SF
         self.preamble_len = preamble_len
+        self.thres = thres
 
-        self.demodulator = css_demod_algo.css_demod_algo(self.M)
+        self.demod = css_demod_algo.css_demod_algo(self.M)
+        self.demod_conj = css_demod_algo.css_demod_algo(self.M, True)
         self.preamble_value = 0
         self.sync_value = [0, 0]
         self.sof_value = 0
 
         #Buffers are initially set to -1
         self.conj_buffer = numpy.zeros(2, dtype=numpy.int) - 1
-        self.buffer = numpy.zeros(preamble_len + 4, dtype=numpy.int) - 1
+        self.buffer = numpy.zeros(preamble_len + 2, dtype=numpy.int) - 1
+
+        self.detect_downchirp0 = False
+        self.detect_downchirp1 = False
 
         self.set_output_multiple(self.M)
 
@@ -57,20 +62,23 @@ class lora_preamble_detect(gr.sync_block):
         if self.buffer[0] == -1:
             return False
 
-        pattern = numpy.repeat(self.buffer[0], self.preamble_len)
-        if (self.buffer[0:self.preamble_len] == pattern).all():
-            self.preamble_value = self.buffer[0]
+        mean = numpy.mean(self.buffer[0:self.preamble_len])
+        mean_err_sq = numpy.sum(numpy.abs(self.buffer[0:self.preamble_len]-mean)**2)
+        max_err_sq = self.M**2
+
+        if(mean_err_sq/max_err_sq < self.thres):
+            self.preamble_value = numpy.uint16(numpy.round(mean))
 
             if self.debug == True:
                 print('Preamble detected: ' + str(self.preamble_value))
 
-            return True
+            self.detect_sync()
+            self.detect_downchirp0 = True
 
-        return False
 
     def detect_sync(self):
-        self.sync_value[0] = numpy.mod(numpy.int16(self.buffer[-4]) - self.preamble_value, self.M)
-        self.sync_value[1] = numpy.mod(numpy.int16(self.buffer[-3]) - self.preamble_value, self.M)
+        self.sync_value[0] = numpy.mod(numpy.int16(self.buffer[-2]) - self.preamble_value, self.M)
+        self.sync_value[1] = numpy.mod(numpy.int16(self.buffer[-1]) - self.preamble_value, self.M)
 
         #First sync value must be different from preamble value
         if self.sync_value[0] < 8:
@@ -81,7 +89,7 @@ class lora_preamble_detect(gr.sync_block):
 
         return True
 
-    def detect_sof(self):
+    def check_downchirps(self):
         if self.conj_buffer[0] == self.conj_buffer[1]:
             self.sof_value = self.conj_buffer[0]
 
@@ -92,39 +100,42 @@ class lora_preamble_detect(gr.sync_block):
 
         return False
 
+    def find_first_symbol(self):
+        pass
+
     def tag_end_preamble(self, sym_idx):
         #Compute time and frequency shift
-        time_shift = (self.preamble_value + self.sof_value)/2
-        freq_shift = (self.preamble_value - self.sof_value)/2
+        time_shift = (self.preamble_value - self.sof_value)/2
+        freq_shift = (self.preamble_value + self.sof_value)/2
 
+        freq_shift = freq_shift%(self.M/2)
         #We cannot correct frequency shifts higher than M/4 and lower than -M/4
-        if(freq_shift >= self.M/4):
-            time_shift -= self.M/2
+        if freq_shift >= self.M/4:
             freq_shift -= self.M/2
-        elif(freq_shift <= -self.M/4):
-            time_shift += self.M/2
-            freq_shift += self.M/2
 
-        #Note: time_shift \in [0; M-1], freq_shift \in [-(M-1)/2 ; (M-1)/2]
+        time_shift = -time_shift
+        if time_shift > numpy.abs(freq_shift):
+            time_shift -= self.M/2
+        elif time_shift < -numpy.abs(freq_shift):
+            time_shift += self.M/2
 
         #Prepare tag
-        #tag_offset = self.nitems_written(0) + self.M*(sym_idx+1) + self.M/4 - time_shift
-        #tag_offset = self.nitems_written(0) + self.M*(sym_idx+1) - time_shift
-        tag_offset = self.nitems_written(0) - time_shift + (sym_idx+1)*self.M + self.M/4
-        if time_shift >= self.M/2:
-            tag_offset += self.M
+        tag_offset = self.nitems_written(0) + time_shift + (sym_idx+1)*self.M + self.M/4
 
         tag1_key = pmt.intern('pkt_start')
         tag1_value = pmt.PMT_NIL
         tag2_key = pmt.intern('freq_offset')
-        tag2_value = pmt.to_pmt(-freq_shift/float(self.M))
+        tag2_value = pmt.to_pmt(freq_shift/float(self.M))
         tag3_key = pmt.intern('sync_word')
         tag3_value = pmt.to_pmt(self.sync_value)
+        tag4_key = pmt.intern('time_offset')
+        tag4_value = pmt.to_pmt(time_shift)
 
         #Append tags
         self.add_item_tag(0, tag_offset, tag1_key, tag1_value)
         self.add_item_tag(0, tag_offset, tag2_key, tag2_value)
         self.add_item_tag(0, tag_offset, tag3_key, tag3_value)
+        self.add_item_tag(0, tag_offset, tag4_key, tag4_value)
 
     def work(self, input_items, output_items):
         in0 = input_items[0]
@@ -133,24 +144,30 @@ class lora_preamble_detect(gr.sync_block):
         #Copy input to output
         out0[:] = in0[:]
 
-        #Demodulate received items
-        syms = self.demodulator.demodulate(in0)
-
-        #Demodulate with up-chirp
-        #(equivalent to non-coherently demodulate conjugate of signal)
-        conj_syms = self.demodulator.demodulate(numpy.conjugate(in0))
-
+        n_syms = len(in0)//self.M
         #Preamble detection
-        for i in range(0, len(syms)):
-            #Shift buffer
+        for i in range(0, n_syms):
+            if self.detect_downchirp1 or self.detect_downchirp0:
+                #Conjugate demod
+                self.conj_buffer = numpy.roll(self.conj_buffer, -1)
+                self.conj_buffer[-1] = self.demod_conj.demodulate(in0[i*self.M:(i+1)*self.M])
+
+            if self.detect_downchirp1:
+                #SOF detection
+                if self.check_downchirps():
+                    self.find_first_symbol()
+                    self.tag_end_preamble(i)
+
+                self.detect_downchirp1 = False
+
+            if self.detect_downchirp0:
+                self.detect_downchirp0 = False
+                self.detect_downchirp1 = True
+
+            #Demod
             self.buffer = numpy.roll(self.buffer, -1)
-            self.buffer[-1] = syms[i]
-
-            #Shift buffer
-            self.conj_buffer = numpy.roll(self.conj_buffer, -1)
-            self.conj_buffer[-1] = conj_syms[i]
-
-            if self.detect_preamble() and self.detect_sync() and self.detect_sof():
-                self.tag_end_preamble(i)
+            self.buffer[-1] = self.demod.demodulate(in0[i*self.M:(i+1)*self.M])
+            #Preamble detection
+            self.detect_preamble()
 
         return len(output_items[0])
