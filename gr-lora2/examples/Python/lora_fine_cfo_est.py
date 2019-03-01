1effffa5e8dac24095560af1edc9512cd867ca71
Alexandre Marquet <alexandre.marquet@imt-atlantique.fr>
2019-03-01 22:01:11 +0100

    Update and rename fine_cfo estimator example script.

--- gr-lora2/examples/Python/lora_fine_cfo_est.py
+++ gr-lora2/examples/Python/lora_fine_cfo_est.py
@@ -14,16 +14,17 @@ import lora2
 
 class lora_sync_test(gr.top_block):
 
-    def __init__(self, SF, n_syms, noise_var, cfo):
+    def __init__(self, SF, n_pkts, n_syms_pkt, noise_var, cfo, delay):
         gr.top_block.__init__(self, "Lora Sync Test")
 
         ##################################################
         # Variables
         ##################################################
         self.SF = SF
-        self.n_syms = n_syms
+        self.n_syms_pkt = n_syms_pkt
         self.noise_var = noise_var
         self.cfo = cfo
+        self.delay = delay
 
         self.interp = 1
         self.M = 2**self.SF
@@ -32,9 +33,16 @@ class lora_sync_test(gr.top_block):
         # Blocks
         ##################################################
         #Modulator
-        syms_vec = numpy.random.randint(0, 2**SF, n_syms, dtype=numpy.uint16)
+        syms_vec = numpy.random.randint(0, 2**SF, n_syms_pkt*n_pkts,
+                dtype=numpy.uint16)
         self.vector_source = blocks.vector_source_s(syms_vec.tolist(), False)
+        self.to_tagged = blocks.stream_to_tagged_stream(gr.sizeof_short, 1,
+                self.n_syms_pkt, 'packet_len')
+        self.add_preamble = lora2.lora_add_preamble(8, 0x12, "packet_len",
+                "sync_word", "payload")
         self.css_mod = lora2.css_mod(self.M, self.interp, 'packet_len')
+        self.add_reversed_chirps = lora2.lora_add_reversed_chirps(self.SF,
+                self.interp, "packet_len", "payload", "rev_chirps")
 
         #Channel (1: noise)
         self.tag_gate = blocks.tag_gate(gr.sizeof_gr_complex)
@@ -47,77 +55,92 @@ class lora_sync_test(gr.top_block):
                 1, 0)
         self.multiplicator = blocks.multiply_vcc(1)
 
-        #Demodulator
-        self.css_demod = grc_css_demod(self.M)
-        self.null_sink = blocks.null_sink(gr.sizeof_short)
-        self.cfo_det = lora2.css_fine_freq_error_detector(self.M, 8)
-        self.msg_sink = blocks.message_debug()
+        #Channel (3: delay)
+        self.delayer = blocks.delay(gr.sizeof_gr_complex, self.delay)
+
+        self.preamble_detector = lora2.lora_preamble_detect(self.SF, 8, debug=False, thres=1e-4)
+        self.store_tags = lora2.store_tags(numpy.complex64, "fine_freq_offset")
 
         ##################################################
         # Connections
         ##################################################
         #Modulator
-        self.connect((self.vector_source, 0), (self.css_mod, 0))
-        self.connect((self.css_mod, 0), (self.tag_gate, 0))
+        self.connect((self.vector_source, 0), (self.to_tagged, 0))
+        self.connect((self.to_tagged, 0), (self.add_preamble, 0))
+        self.connect((self.add_preamble, 0), (self.css_mod, 0))
+        self.connect((self.css_mod, 0), (self.add_reversed_chirps, 0))
+        self.connect((self.add_reversed_chirps, 0), (self.tag_gate, 0))
 
         #Channel
         self.connect((self.tag_gate, 0), (self.adder, 1))
         self.connect((self.noise_source, 0), (self.adder, 0))
         self.connect((self.adder, 0), (self.multiplicator, 1))
         self.connect((self.cfo_source, 0), (self.multiplicator, 0))
-        self.connect((self.multiplicator, 0), (self.css_demod, 0))
+        self.connect((self.multiplicator, 0), (self.delayer, 0))
+        self.connect((self.delayer, 0), (self.preamble_detector, 0))
 
         #Demodulator
-        self.connect((self.css_demod, 0), (self.null_sink, 0))
-        self.connect((self.css_demod, 1), (self.cfo_det, 0))
-        self.msg_connect((self.cfo_det, "freq"), (self.msg_sink, "store"))
-
+        self.connect((self.preamble_detector, 0), (self.store_tags, 0))
+ 
 if __name__ == "__main__":
     #Parameters
     SF = 9
-    n_syms = 8
+    n_pkts = 1
+    n_bytes = 1*SF
+    n_syms = 8*n_bytes/SF
     M = 2**SF
 
     #EbN0dB = numpy.linspace(0, 10, 11)
-    EbN0dB = numpy.array([100])
+    EbN0dB = numpy.array([10])
     Eb = 1.0/M
     N0=Eb * 10**(-EbN0dB/10.0)
     noise_var=(M**2 * N0)/numpy.log2(M)
+    time_offset = 0
 
     cfo_min = -0.5/M
     cfo_max = 0.5/M
     cfos = numpy.linspace(cfo_min, cfo_max, 50)
 
     #CFO impact
-    est_cfo = numpy.zeros((len(cfos), len(EbN0dB)))
+    cfo_est = numpy.zeros((len(cfos), len(EbN0dB)))
     for j in range(0, len(cfos)):
         print('CFO = ' + str(cfos[j]))
         for i in range(0, len(EbN0dB)):
             #Setup block
-            tb = lora_sync_test(SF, n_syms, noise_var[i], cfos[j])
+            tb = lora_sync_test(SF, n_pkts, n_syms, noise_var[i], cfos[j], time_offset)
 
             #Simulate
             tb.start()
             tb.wait()
 
-            if tb.msg_sink.num_messages() == 0:
-                print('No message received')
+            #Retrieve
+            tags = tb.store_tags.get_tags()
+
+            #Compute error
+            mean_est_cfo = 0.0
+            if len(tags) > 0:
+                for tag in tags:
+                    mean_est_cfo += pmt.to_float(tag.value)
+                mean_est_cfo /= len(tags)
             else:
-                for k in range(0, tb.msg_sink.num_messages()):
-                    msg = tb.msg_sink.get_message(k)
-                    est_cfo[j,i] -= pmt.to_float(msg)
+                mean_est_cfo = 10.0
+                print('No tag detected.')
 
-                est_cfo[j,i] /= tb.msg_sink.num_messages()
+            if len(tags) > n_pkts:
+                print('Multiple tags detected.')
 
-            print('Estimated cfo at EbN0dB = ' + str(EbN0dB[i]) + ': '
-                    + str(est_cfo[j,i]))
+            #Detected / total ratio
+            cfo_est[j,i] = mean_est_cfo
+
+            print('Estimated CFO Eb/N0 (dB)=' + str(EbN0dB[i]) + ' -> '
+                    + str(mean_est_cfo))
 
             del tb
 
     plt.figure()
     plt.plot(cfos, cfos, label='Ref')
     for j in range(0, len(EbN0dB)):
-        plt.plot(cfos, est_cfo[:,j], label=str(EbN0dB[j]), marker='+')
+        plt.plot(cfos, cfo_est[:,j], label=str(EbN0dB[j]), marker='+')
 
 
     plt.title('Fine CFO estimator')
