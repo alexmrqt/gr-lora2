f1b4182a78e61622605ba2aeafa55aa2d83ef2cf
Alexandre Marquet <alexandre.marquet@imt-atlantique.fr>
2021-01-15 16:52:00 +0100

    Documentation for lora_pad

--- gr-lora2/include/lora2/lora_pad.h
+++ gr-lora2/include/lora2/lora_pad.h
@@ -28,7 +28,29 @@ namespace gr {
 namespace lora2 {
 
 /*!
- * \brief <+description of block+>
+ * \brief Block to pad a LoRa packet with zeros.
+ *
+ * This tagged stream block appends zeros at the end of LoRa packets, in order
+ * to match their length with the input block sizes expected by the LoRa
+ * interleaver: `SF.(CR+4)` binary elements (or `(SF-2).(CR+4)` binary elements
+ * if `reduced_rate == true`).
+ *
+ * The input-output relationship is as follows: let
+ * \f$ (in[0] \ldots in[L-1]) \in [0;1]^{L} \f$ be the input block,
+ * then the output block is given as:
+ * \f[
+ * 		(in[0] \ldots in[L-1], 0 \ldots 0) \in [0;1]^{L+L_{pad}}
+ * \f].
+ * The number of zeros appended to the packet is given as:
+ * \f[
+ * 		L_{pad} = \left\{
+ * 		\begin{array}{ll}
+ * 			0 & {\rm if} \quad L \: {\rm mod} \: (SF.(4+CR)) = 0 \\
+ *	 		SF.(4+CR) - L \: {\rm mod} \: (SF.(4+CR)) & {\rm if} \quad reduced\_rate = false \\
+ *	 		(SF-2).(4+CR) - L \: {\rm mod} \: ((SF-2).(4+CR)) & {\rm otherwise}
+ *	 	\end{array} \right.
+ * \f]
+ *
  *
  */
 class LORA2_API lora_pad : virtual public gr::tagged_stream_block
@@ -39,10 +61,10 @@ class LORA2_API lora_pad : virtual public gr::tagged_stream_block
 		/*!
 		 * \brief Return a shared_ptr to a new instance of lora2::lora_pad.
 		 *
-		 * To avoid accidental use of raw pointers, lora2::lora_pad's
-		 * constructor is in a private implementation
-		 * class. lora2::lora_pad::make is the public interface for
-		 * creating new instances.
+		 * \param len_tag_key Length tag key for the tagged stream.
+		 * \param SF LoRa spreading factor.
+		 * \param CR LoRa coding rate (between 1 and 4).
+		 * \param reduced_rate Set to true for LoRa low datarate optimization.
 		 */
 		static sptr make(const std::string &len_tag_key, unsigned char SF,
 				unsigned char CR, bool reduced_rate = false);
