c324a75833f414b8585feeaf36ea4631511b648c
Alexandre Marquet <alexandre.marquet@imt-atlantique.fr>
2021-02-22 22:14:20 +0100

    Documentation for lora_hamming_decode

--- gr-lora2/include/lora2/lora_hamming_encode.h
+++ gr-lora2/include/lora2/lora_hamming_encode.h
@@ -28,7 +28,34 @@ namespace gr {
 namespace lora2 {
 
 /*!
- * \brief <+description of block+>
+ * \brief LoRa Hamming encoder.
+ * 
+ * The LoRa Hamming encoder is a systematic block error correcting codre,
+ * acting on input blocks of 4 bits, and producing encoded blocks of 4+CR bits,
+ * with \f$CR \in \{1,2,3,4\}\f$.
+ * Consequently, the code has an efficiency \f$\eta = 4/(CR+4)\f$.
+ *
+ * Denoting \f$in[n] \in [0,1] \f$ the \f$n\f$-th input sample, the \f$k\f$-th
+ * input block is defined as \f$(in[4k] \dots in[4k+3]) \in [0;1]^4\f$.
+ * Similarly, the \f$k\f$-th output block is given as:
+ * \f{align*}{
+ * 	&(p_0, in_k[4k] \dots in[4k+3]) \: \mbox{ for } CR=1 \\
+ * 	&(p_2, p_1, in[4k] \dots in[4k+3]) \: \mbox{ for } CR=2 \\
+ * 	&(p_3, p_2, p_1, in[4k] \dots in[4k+3]) \: \mbox{ for } CR=3 \\
+ * 	&(p_3, p_4, p_2, p_1, in[4k] \dots in[4k+3]) \: \mbox{ for } CR=4
+ * \f}
+ * Where, denoting \f$a \oplus b\f$ the exclusive-or (XOR) operation betwee,
+ * \f$ a, b \in[0;1] \f$ :
+ * \f{align*}{
+ * 	p_0 &= in[4k] \oplus in[4k+1] \oplus in[4k+2] \oplus in[4k+3]\\
+ * 	p_1 &= in[4k+1] \oplus in[4k+2] \oplus in[4k+3]\\
+ * 	p_2 &= in[4k] \oplus in[4k+1] \oplus in[4k+2] \\
+ * 	p_3 &= in[4k] \oplus in[4k+1] \oplus in[4k+3] \\
+ * 	p_4 &= in[4k] \oplus in[4k+2] \oplus in[4k+3]
+ * \f}
+ *
+ * This block is not a tagged stream block, but it will update the length tag
+ * identified by contructor paramter `len_tag_key`, if any (and if supplied).
  *
  */
 class LORA2_API lora_hamming_encode : virtual public gr::block
@@ -39,10 +66,8 @@ class LORA2_API lora_hamming_encode : virtual public gr::block
 		/*!
 		 * \brief Return a shared_ptr to a new instance of lora2::lora_hamming_encode.
 		 *
-		 * To avoid accidental use of raw pointers, lora2::lora_hamming_encode's
-		 * constructor is in a private implementation
-		 * class. lora2::lora_hamming_encode::make is the public interface for
-		 * creating new instances.
+		 * \param CR Coding rate (between 1 and 4).
+		 * \param len_tag_key Length tag key to be updated.
 		 */
 		static sptr make(int CR, const std::string &len_tag_key);
 };
