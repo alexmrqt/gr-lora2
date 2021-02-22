d05e1b62379e93fc597523b5f431684096cc1018
Alexandre Marquet <alexandre.marquet@imt-atlantique.fr>
2021-02-22 22:45:44 +0100

    Documentation for lora_hamming_decode

--- gr-lora2/include/lora2/lora_hamming_decode.h
+++ gr-lora2/include/lora2/lora_hamming_decode.h
@@ -28,8 +28,41 @@ namespace gr {
 namespace lora2 {
 
 /*!
- * \brief <+description of block+>
+ * \brief LoRa Hamming decoder.
  *
+ * This block implements a syndrome decoder (a.k.a: maximum likelihood hard
+ * decoder) for the LoRa hamming code.
+ *
+ * Denoting \f$in[n] \in [0,1] \f$ the \f$n\f$-th input sample, the \f$k\f$-th
+ * input block is defined as
+ * \f$(in[4k] \dots in[4k+CR+3]) = (in_k[0], \dots in_k[CR+3]) \in [0;1]^{CR+4}\f$.
+ * The syndrome \f$ s_k\in[0;1]^{CR} \f$ of this input block is computed as:
+ * \f{align*}{
+ * 	s_k &= (in_k[0] \oplus in_k[1] \oplus in_k[2] \oplus in_k[3] \oplus in_k[4])
+ *	 	\: \mbox{ for } CR=1 \\
+ * 	s_k &= (in_k[0] \oplus in_k[2] \oplus in_k[3] \oplus in_k[4],
+ * 		in_k[1] \oplus in_k[3] \oplus in_k[4] \oplus in_k[5])
+ * 		\: \mbox{ for } CR=2 \\
+ * 	s_k &= (in_k[0] \oplus in_k[3] \oplus in_k[4] \oplus in_k[6],
+ * 		in_k[1] \oplus in_k[3] \oplus in_k[4] \oplus in_k[5],
+ * 		in_k[2] \oplus in_k[4] \oplus in_k[5] \oplus in_k[6])
+ * 		\: \mbox{ for } CR=3 \\
+ * 	s_k &= (in_k[0] \oplus in_k[5] \oplus in_k[5] \oplus in_k[7],
+ * 		in_k[1] \oplus in_k[4] \oplus in_k[6] \oplus in_k[7],
+ * 		in_k[2] \oplus in_k[4] \oplus in_k[5] \oplus in_k[6],
+ * 		in_k[3] \oplus in_k[5] \oplus in_k[6] \oplus in_k[7])
+ * 		\: \mbox{ for } CR=4
+ * \f}
+ * Each syndrome is associated with, of all \f$2^{CR+4}\f$possible error vectors,
+ * the one having the lowest hammming weight (that is, the lower number of ones).
+ * It is denoted \f$e(s_k) = (e_0 \dots e_{CR+3}) \in [0;1]^{CR+4}\f$.
+ * Using the error vector, the output block is computed as:
+ * \f[
+ * 	(in[4k+CR] \oplus e_0 \dots in[4k+CR+3] \oplus e_3) \: \in [0;1]^4
+ * \f]
+ *
+ * This block is not a tagged stream block, but it will update the length tag
+ * identified by contructor paramter `len_tag_key`, if any (and if supplied).
  */
 class LORA2_API lora_hamming_decode : virtual public gr::block
 {
@@ -39,10 +72,8 @@ class LORA2_API lora_hamming_decode : virtual public gr::block
 		/*!
 		 * \brief Return a shared_ptr to a new instance of lora2::lora_hamming_decode.
 		 *
-		 * To avoid accidental use of raw pointers, lora2::lora_hamming_decode's
-		 * constructor is in a private implementation
-		 * class. lora2::lora_hamming_decode::make is the public interface for
-		 * creating new instances.
+		 * \param CR Coding rate (between 1 and 4).
+		 * \param len_tag_key Length tag key to be updated.
 		 */
 		static sptr make(int CR, const std::string &len_tag_key);
 };
