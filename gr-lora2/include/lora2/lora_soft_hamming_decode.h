ddcbb3b6c0f54e9de2a2155240c5208fcfdc49ef
Alexandre Marquet <alexandre.marquet@imt-atlantique.fr>
2021-03-01 22:33:53 +0100

    Documentation for lora_soft_hamming_decode

--- gr-lora2/include/lora2/lora_soft_hamming_decode.h
+++ gr-lora2/include/lora2/lora_soft_hamming_decode.h
@@ -28,8 +28,27 @@ namespace gr {
 namespace lora2 {
 
 /*!
- * \brief <+description of block+>
+ * \brief LoRa soft Hamming decoder.
  *
+ * This block implements a brute force maximum likelihood decoder for the LoRa
+ * Hamming code.
+ *
+ * Let us denote \f$in[n] \in \mathbb{R} \f$ the \f$n\f$-th input sample.
+ * The \f$k\f$-th input block is then defined as: 
+ * \f$(in[4k] \dots in[4k+CR+3]) = (in_k[0], \dots in_k[CR+3]) = \mathbf{in}_k \in \mathbb{R}^{CR+4}\f$.
+ * We also denote \f$ enc\{\mathbf{b}\} = (b[0] \dots b[3]) \in [0;1]^{CR+4} \f$
+ * the Hamming encoding of the block \f$\mathbf{b} \in [0;1]^4\f$.
+ * Then, this block decodes \f$ \mathbf{in}_k \f$ as:
+ * \f[
+ * \mathbf{out}_k = \mathrm{argmin}_{\mathbf{\bar{\mathrm{b}}} \in [0;1]^4}
+ * \sum_{l=0}^{CR+3} enc\{\mathbf{\bar{\mathrm{b}}}\}[l].in_k[l]
+ * \f]
+ * where \f$ \mathbf{out}_k = (out_k[0] \dots out_k[3]) = (out[3k] \dots out[3k+3]) \in [0;1]^4\f$
+ * is the \f$k\f$-th output block,
+ * and \f$out[n] \in [0;1]\f$ is the \f$n\f$-th output sample.
+ *
+ * This block is not a tagged stream block, but it will update the length tag
+ * identified by contructor paramter `len_tag_key`, if any (and if supplied).
  */
 class LORA2_API lora_soft_hamming_decode : virtual public gr::block
 {
@@ -39,10 +58,8 @@ class LORA2_API lora_soft_hamming_decode : virtual public gr::block
 		/*!
 		 * \brief Return a shared_ptr to a new instance of lora2::lora_soft_hamming_decode.
 		 *
-		 * To avoid accidental use of raw pointers, lora2::lora_soft_hamming_decode's
-		 * constructor is in a private implementation
-		 * class. lora2::lora_soft_hamming_decode::make is the public interface for
-		 * creating new instances.
+		 * \param CR Coding rate (between 1 and 4).
+		 * \param len_tag_key Length tag key to be updated.
 		 */
 		static sptr make(int CR, const std::string &len_tag_key);
 };
