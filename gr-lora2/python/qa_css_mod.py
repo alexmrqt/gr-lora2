04591da8abac4ee4a892af3850c6b2a8bf4da4bf
Alexandre Marquet <alexandre.marquet@imt-atlantique.fr>
2022-02-23 17:32:21 +0100

    Update all dates

--- gr-lora2/python/qa_css_mod.py
+++ gr-lora2/python/qa_css_mod.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # 
-# Copyright 2019 Alexandre Marquet.
+# Copyright 2022 Alexandre Marquet.
 # 
 # This is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
